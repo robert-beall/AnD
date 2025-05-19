305653328131b30747ebfaa7cf04894bb03befdf
Robert J Beall <rbeall@smcm.edu>
2025-05-19 10:07:18 -0400

    Update

---
+++ DataStructures/linked-list/implementations/c++/ll.cpp
@@ -0,0 +1,8 @@
+#include "ll.h"
+#include <iostream>
+
+using namespace LL;
+
+linked_list::linked_list(int e) {
+	data = e;
+}
---
+++ DataStructures/linked-list/implementations/c++/ll.h
@@ -0,0 +1,10 @@
+namespace LL {
+	class linked_list {
+		public: 
+			int data;
+			linked_list* next;
+                        linked_list(int e);
+			void insert();
+			int remove();
+	};
+}
